18ea5361cee65c624e6dcc37847b813d1652c7c3
Sharon Shabtai <sharons@spectralogic.com>
2016-10-20 15:58:37 -0600

    first commit

---
+++ TpfrClient/PluginSdk.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * ******************************************************************************
+ *   Copyright 2014 - 2016 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace TpfrClient
+{
+    public class PluginSdk : IPluginSdk
+    {
+        private INetwork _network;
+
+        public PluginSdk(string hostServerName, int hostServerPort)
+        {
+            _network = new Network(hostServerName, hostServerPort);
+        }
+
+        public PluginSdk(INetwork network)
+        {
+            _network = network;
+        }
+
+        public Status IndexFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Status IndexStatus(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ByteRange> QuestionTimecode(string clipName, string indexFilePath,
+            IEnumerable<TimecodeRange> timecodes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReWrap(string fileToProcessPath, string indexFilePath, IEnumerable<TimecodeRange> timecodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TpfrClientTest/TpfrClientTest.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * ******************************************************************************
+ *   Copyright 2014 - 2016 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+using NUnit.Framework;
+using TpfrClient;
+
+namespace TpfrClientTest
+{
+    [TestFixture]
+    public class TpfrClientTest
+    {
+        private IPluginSdk _pluginSdk;
+
+        [SetUp]
+        public void Setup()
+        {
+            _pluginSdk = new PluginSdk(new MockNetwork());
+        }
+
+        [Test]
+        public void TestIndexFile()
+        {
+            _pluginSdk.IndexFile(null);
+        }
+
+        [Test]
+        public void TestIndexStatus()
+        {
+            _pluginSdk.IndexStatus(null);
+        }
+
+        [Test]
+        public void TestQuestionTimecode()
+        {
+            _pluginSdk.QuestionTimecode(null, null, null);
+        }
+
+        [Test]
+        public void TestReWrap()
+        {
+            _pluginSdk.ReWrap(null, null, null);
+        }
+    }
+}
